05b1ee2067e50307709e685c12de354e857a8b16
Xeno Fish <xfish@ups.edu>
2018-01-30 14:52:18 -0800

    directory error

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var ctrlArt = require('../controllers/art');
-var ctrl = require('../controllers/Cs');
+var ctrlArt = require('../controllers/art.js');
+var ctrl = require('../controllers/Cs.js');
 /* GET home page. */
 router.get('/', ctrl.index);
 
