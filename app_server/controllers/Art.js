d6d369c7308a7ae73188e77b3d4dbc332d6b7059
Xeno Fish <xfish@ups.edu>
2018-02-02 09:07:47 -0800

    fully integrated database

--- app_server/controllers/Art.js
+++ app_server/controllers/Art.js
@@ -1,8 +1,59 @@
-
+var request = require('request');
+var apiOptions = {
+  server : "http://localhost:3000"
+};
+if (process.env.NODE_ENV === 'production') {
+  apiOptions.server = "https://colinxenofish.herokuapp.com/";
+}
 /* GET art page */
 module.exports.art = function(req, res){
-  res.render('art', { title: 'Art' });
+  var requestOptions, path;
+  path = "/api/pieces";
+   requestOptions= {
+     url: apiOptions.server+path,
+     method: "GET",
+     json:{},
+     qs:{}
+   };
+   request(
+     requestOptions,
+     function(err,response,body){
+       renderArtList(req,res,body);
+     });
 };
-module.exports.elk = function(req, res){
-  res.render('elk', { title: 'Elk' , locs: ['elk2','elk1'],});
+var renderArtList = function (req,res,responseBody){
+res.render('art', { title: 'Art', art: responseBody});
+};
+
+
+var getPieceInfo = function (req, res, callback) {
+  var requestOptions, path;
+  path = "/api/pieces/" + req.params.pieceid;
+  requestOptions = {
+    url : apiOptions.server + path,
+    method : "GET",
+    json : {}
+  };
+  request(
+    requestOptions,
+    function(err, response, body) {
+      var data = body;
+      if (response.statusCode === 200) {
+        callback(req, res, data);
+      } else {
+        _showError(req, res, response.statusCode);
+      }
+    }
+  );
+};
+
+var renderDetailPage = function (req, res, info) {
+res.render('PieceInfo', { title: info.title, info});
+};
+
+/* GET 'Location info' page */
+module.exports.pieceInfo = function(req, res){
+  getPieceInfo(req, res, function(req, res, responseData) {
+    renderDetailPage(req, res, responseData);
+  });
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,7 +4,6 @@ var ctrlArt = require('../controllers/Art');
 var ctrl = require('../controllers/Cs');
 /* GET home page. */
 router.get('/', ctrl.index);
-
 router.get('/Art', ctrlArt.art);
-router.get('/Art/Elk', ctrlArt.elk);
+router.get('/Art/:pieceid', ctrlArt.pieceInfo);
 module.exports = router;
